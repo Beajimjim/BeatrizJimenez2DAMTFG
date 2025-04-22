3a2f40cc863faea62ba13b0ef3d84c3f369aedd6
beajim01 <beajimjim93@gmail.com>
2025-04-22 20:51:50 +0200

    modal

--- src/app/pages/escritorio/nuevo-proyecto-modal/nuevo-proyecto-modal.component.ts
+++ src/app/pages/escritorio/nuevo-proyecto-modal/nuevo-proyecto-modal.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevo-proyecto-modal',
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonicModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './nuevo-proyecto-modal.component.html',
-  styleUrls: ['./nuevo-proyecto-modal.component.scss'],
+  styleUrls: ['./nuevo-proyecto-modal.component.scss']
 })
-export class NuevoProyectoModalComponent  implements OnInit {
+export class NuevoProyectoModalComponent {
+  formProyecto: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    // Definimos los campos
+    this.formProyecto = this.fb.group({
+      nombre: [''],
+      fecha: [''],   // aquí guardamos la fecha
+      jefe: [''],
+      equipo: ['']
+    });
+  }
 
-  ngOnInit() {}
+  cerrarModal() {
+    // Aquí cierras el modal, por ejemplo con modalCtrl.dismiss()
+    console.log('Modal cerrado');
+  }
 
+  guardarProyecto() {
+    // Recoges los datos del formulario
+    const datos = this.formProyecto.value;
+    console.log('Datos del proyecto:', datos);
+    // Podrías hacer una petición a tu backend, etc.
+    this.cerrarModal();
+  }
 }
